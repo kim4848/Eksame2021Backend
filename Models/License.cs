dac97930a0e1695e87525d0a9c4f937de0479bf7
Kim <kj@kjsoft.dk>
2021-11-05 16:30:22 +0100

    Rettet licens model

--- Models/License.cs
+++ Models/License.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -58,11 +59,11 @@ namespace License.Models
 
     public class IncludedFeaturesWrapper
     {
-        public List<StandardFeature> StandardFeatures { get; set; } = new List<StandardFeature>();
-        public List<EsdhIntegration> EsdhIntegrations { get; set; } = new List<EsdhIntegration>();
-        public List<Addon> Addons { get; set; } = new List<Addon>();
-        public List<LookupIntegration> LookupIntegrations { get; set; } = new List<LookupIntegration>();
-        public List<OutputManagementIntegration> OutputManagementIntegrations { get; set; } = new List<OutputManagementIntegration>();
+        public StandardFeature StandardFeatures { get; set; } = new StandardFeature();
+        public EsdhIntegration EsdhIntegrations { get; set; } = new EsdhIntegration();
+        public Addon Addons { get; set; } = new Addon();
+        public LookupIntegration LookupIntegrations { get; set; } = new LookupIntegration();
+        public OutputManagementIntegration OutputManagementIntegrations { get; set; } = new OutputManagementIntegration();
     }
 
     public abstract class FeatureDTOBase
@@ -93,158 +94,68 @@ namespace License.Models
 
     #region Enums
 
-    public enum StandardFeature
+    public class StandardFeature
     {
-        [Description("ComboBlocks")]
-        ComboBlock = 1, // Tidligere ConcatenatePhrases
-        [Description("OneClick templates")]
-        OneClickDocuments = 2,
-        [Description("Printer tray selection")]
-        PrinterTraySelection = 3,
-        [Description("Resource management")]
-        ResourceAdministration = 4,
-        [Description("Send as PDF")]
-        SendAsPdf = 5,
-        [Description("Signature management")]
-        SignatureAdministration = 6,
-        [Description("Standard Content")]
-        StandardContent = 7, // Tidligere Phrases
-        [Description("Standard Content creation")]
-        StandardContentCreation = 8, // Tidligere PhraseCreation
-        [Description("Templates")]
-        Templates = 9,
-        [Description("Local user profiles")]
-        UserAdministration = 10,
-        [Description("Offline mode")]
-        Offline = 11
+
+        public bool ComboBlock { get; set; }
+        public bool PrinterTraySelection { get; set; }
+        public bool ResourceAdministration { get; set; }
+        public bool SendAsPdf { get; set; }
+        public bool SignatureAdministration { get; set; }
+        public bool StandardContent { get; set; }
+        public bool StandardContentCreation { get; set; }
+        public bool Templates { get; set; }
+        public bool UserAdministration { get; set; }
+        public bool Offline { get; set; }
     }
 
-    public enum EsdhIntegration
+    public class EsdhIntegration
     {
-        [Description("Abakion Legal")]
-        AbakionLegal = 0,
-        [Description("Acadre")]
-        AcadreIntegration = 1,
-        [Description("Acos WebSak")]
-        AcosWebSakIntegration = 3,
-        [Description("cBrain F2")]
-        cBrainF2Integration = 4,
-        [Description("Ciceron")]
-        CiceronIntegration = 5,
-        [Description("Columna Cura")]
-        ColumnaCuraIntegration = 6,
-        [Description("CSC Vitae")]
-        CSCVitaeIntegration = 7,
-        [Description("D4")]
-        D4Phrases = 8,
-        [Description("DUBU 3")]
-        DUBU3Integration = 10,
-        [Description("Dynamics CRM")]
-        DynamicsCRMIntegration = 11,
-        [Description("Dynamics CRM (udvidet GI)")]
-        DynamicsCRMIntegrationExtendedGI = 12,
-        [Description("eDoc")]
-        eDocIntegration = 13,
-        [Description("EDP Vision")]
-        EDPVision = 14,
-        [Description("EG Sensum")]
-        EGSensumCCIntegration = 15,
-        [Description("EG Sensum (Deprecated)")]
-        EGSensumIntegration = 16,
-        [Description("Elements")]
-        ElementsIntegration = 17,
-        [Description("ePhorte")]
-        ePhorteIntegration = 18,
-        [Description("Evry ESA")]
-        EvryESAIntegration = 19,
-        [Description("FICS")]
-        FICSIntegration = 20,
-        [Description("GetOrganized")]
-        GetOrganizedIntegration = 21,
-        [Description("Idox Uniform")]
-        IdoxUniformIntegration = 22,
-        [Description("Information@Work")]
-        InformationAtWorkIntegration = 23,
-        [Description("KMD Care")]
-        KMDCareIntegration = 24,
-        [Description("KMD Momentum")]
-        KMDMomentum = 25,
-        [Description("KMD Nexus")]
-        KMDNexusIntegration = 26,
-        [Description("KMD Nova")]
-        KMDNovaIntegration = 27,
-        [Description("KMD Sag")]
-        KMDSagIntegration = 28,
-        [Description("Modus")]
-        ModusIntegration = 29,
-        [Description("Novax")]
-        NovaxIntegration = 30,
-        [Description("Platina")]
-        PlatinaIntegration = 31,
-        [Description("Profile")]
-        ProfileIntegration = 32,
-        [Description("Public 360")]
-        Public360Integration = 33,
-        [Description("SBSYS")]
-        SBSYSIntegration = 35,
-        [Description("TK2")]
-        TK2Integration = 36,
-        [Description("W3D3")]
-        W3D3Integration = 37,
-        [Description("Custom XML file (configurable)")]
-        XMLFileIntegration = 38,
+        public bool AbakionLegal { get; set; }
+        public bool AcadreIntegration { get; set; }
+        public bool ComboBlock { get; set; }
+        public bool AcosWebSakIntegration { get; set; }
+        public bool cBrainF2Integration { get; set; }
+        public bool CiceronIntegration { get; set; }
+        public bool ColumnaCuraIntegration { get; set; }
+        public bool KMDMomentum { get; set; }
+        public bool NovaxIntegration { get; set; }
+        public bool SBSYSIntegration { get; set; }
+
     }
 
-    public enum Addon
+    public class Addon
     {
-        [Description("Cloud Statistics access")]
-        CloudStatistics = 1,
-        [Description("Batchlookup of danish CPR/CVR/P-no. in merge template")]
-        MergeSourceLookup = 2,
-        [Description("Prepare any Excel sheet for merging")]
-        MergeSourcePrepare = 3,
-        [Description("Rule field (Remember to maintain 'Holidays.xml' to calculate working days)")]
-        RuleField = 4,
-        [Description("DynamicTemplate Cloud")]
-        DtCloud = 5
+        public bool CloudStatistics { get; set; }
+        public bool MergeSourceLookup { get; set; }
+        public bool MergeSourcePrepare { get; set; }
+        public bool RuleField { get; set; }
+        public bool SBSYSIntegration { get; set; }
+        public bool DtCloud { get; set; }
+
     }
 
-    public enum LookupIntegration
+    public class LookupIntegration
     {
-        [Description("CVR Online")]
-        CvrOnline = 0,
-        [Description("P-Data")]
-        PData = 1,
-        [Description("Serviceplatformen CVR Online 3.0")]
-        SPCvrOnline = 2,
-        [Description("Serviceplatformen FAMILIE+")]
-        SpFamiliePlus = 3,
-        [Description("Serviceplatformen Person Stamdata udvidet")]
-        SpPersonStamdata = 4,
-        [Description("Serviceplatformen STAM+")]
-        SpStamPlus = 5,
-        [Description("Custom SQL database (configurable)")]
-        SqlDB = 6
+        public bool CvrOnline { get; set; }
+        public bool PData { get; set; }
+        public bool SPCvrOnline { get; set; }
+        public bool SpFamiliePlus { get; set; }
+        public bool SpPersonStamdata { get; set; }
+
     }
 
-    public enum OutputManagementIntegration
+    public class OutputManagementIntegration
     {
-        [Description("Convergens I/O Manager")]
-        ConvergensIOManager = 0,
-        [Description("OneTooX")]
-        Doc2Mail = 1,
-        [Description("Mina Meddelanden")]
-        MinaMeddelanden = 2,
-        [Description("Print via Serviceplatformen")]
-        PrintViaServiceplatformen = 3,
-        [Description("Send og journaliser")]
-        SBSend = 4,
-        [Description("SBSIP")]
-        SBSIP = 5,
-        [Description("Strålfors Connect")]
-        StraalforsConnect = 6,
-        [Description("KS SvarUT")]
-        SvarUt = 7
+        public bool ConvergensIOManager { get; set; }
+        public bool Doc2Mail { get; set; }
+        public bool SpPersonStamdata { get; set; }
+        public bool MinaMeddelanden { get; set; }
+        public bool PrintViaServiceplatformen { get; set; }
+        public bool SBSend { get; set; }
+        public bool StraalforsConnect { get; set; }
+        public bool SvarUt { get; set; }
+
     }
 
     #endregion
